0e5a948dbcf94bc1e1bbded1f8a53b315bd336c3
ChinthaShanmukhavani <shanmukhavani.chintha@kansocloud.com>
2024-11-03 22:32:46 +0530

    episode5/lets__get_hooked

--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,23 +1,4 @@
-// import { RECIPE_IMAGE } from "../utils/constants"
-// const ResCard = (props) =>{
-//     const {data} = props
-//     console.log('props',props)
-//     const {name,cuisines,avgRating,deliveryTime} = data?.info
-    
-//     return(
-//       <div className="res-card">
-//         <div className="image-container">
-//           <img id="res-logo" src = {RECIPE_IMAGE+data.info.cloudinaryImageId}/>
-//         </div>
-//         <h3>{name}</h3>
-//         <h5>{cuisines.join(", ")}</h5>
-//         <h5>{avgRating}</h5>
-//         <h5>{deliveryTime}</h5>
-        
-//       </div>
-//     )
-// }
-// export default ResCard
+
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 const ResCard = (props) =>{
   const {data} =props
